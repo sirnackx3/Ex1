45bdd23cc1e3fca1e0f9f0f82d08010aa2141828
sirnackx3 <61181007@g.cmru.ac.th>
2021-02-13 11:57:27 +0700

    เพิ่มไฟล์ about contact

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ex1</title>
+    <title>Document</title>
 </head>
 <body>
-<h1>Homepage</h1>
-<a href="/">หน้าแรก</a> | <a href="/about">About</a> | <a href="/contact">Contact</a>
+<h1>About site</h1>
 </body>
 </html>
